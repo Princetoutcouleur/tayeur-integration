bda6b967376e4ef703ca0c64ea8bd09d6dfaa422
Princetoutcouleur <babathiam0000@gmail.com>
2023-09-14 12:06:21 +0000

    resizing card

--- src/Components/reutilusables/CardAtelier.jsx
+++ src/Components/reutilusables/CardAtelier.jsx
@@ -1,42 +1,50 @@
 import React from "react";
 import { AiFillStar } from "react-icons/ai";
-import { BsWhatsapp, BsTelephoneFill, BsFillChatLeftTextFill } from "react-icons/bs";
+import {
+  BsWhatsapp,
+  BsTelephoneFill,
+  BsFillChatLeftTextFill,
+} from "react-icons/bs";
 
 const CardAtelier = (props) => {
   return (
     <div>
-      <div id="CardAtelier" class="card shadow-lg mb-3">
-      <img src={props.image} alt="" className="img-fluid" />
+      <div id="CardAtelier" class="card shadow-lg mb-3 ">
+        <div className="card-img-top d-flex justify-content-center align-items-center">
+          <div>
+            <img src={props.image} alt="" className="img-fluid rounded-top" />
+          </div>
+        </div>
         <div class="card-body">
-        <p className="text-uppercase fw-bold">{props.title}</p>
+          <p className="text-uppercase fw-bold">{props.title}</p>
           <div>
             <div className="avis d-flex gap-2 align-items-baseline">
               <p className="fs-6">5</p>
-              <AiFillStar className="fs-6 text-warning"/>
-              <AiFillStar className="fs-6 text-warning"/>
-              <AiFillStar className="fs-6 text-warning"/>
-              <AiFillStar className="fs-6 text-warning"/>
-              <AiFillStar className="fs-6 text-warning"/>
+              <AiFillStar className="fs-6 text-warning" />
+              <AiFillStar className="fs-6 text-warning" />
+              <AiFillStar className="fs-6 text-warning" />
+              <AiFillStar className="fs-6 text-warning" />
+              <AiFillStar className="fs-6 text-warning" />
               <p className="fs-6">1 avis</p>
             </div>
             <div>
-                <ul className=" list-unstyled d-flex gap-3">
-                    <li>
-                        <button className="btn btn-success text-light pb-2 rounded-circle">
-                        <BsWhatsapp/>
-                        </button>
-                    </li>
-                    <li>
-                        <button className="btn btn-danger text-light pb-2 rounded-circle">
-                        <BsTelephoneFill/>
-                        </button>
-                    </li>
-                    <li>
-                        <button className="btn btn-danger text-light pb-2 rounded-circle">
-                        <BsFillChatLeftTextFill/>
-                        </button>
-                    </li>
-                </ul>
+              <ul className=" list-unstyled d-flex gap-3">
+                <li>
+                  <button className="btn btn-success text-light pb-2 rounded-circle">
+                    <BsWhatsapp />
+                  </button>
+                </li>
+                <li>
+                  <button className="btn btn-danger text-light pb-2 rounded-circle">
+                    <BsTelephoneFill />
+                  </button>
+                </li>
+                <li>
+                  <button className="btn btn-danger text-light pb-2 rounded-circle">
+                    <BsFillChatLeftTextFill />
+                  </button>
+                </li>
+              </ul>
             </div>
           </div>
         </div>
